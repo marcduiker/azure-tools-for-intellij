8215dd916b8dd60e9136aa5e2732a6bfd5e438f4
Leonid Shalupov <leonid@shalupov.com>
2018-04-30 16:19:13 +0200

    Initial buildscripts for IDEA/Rider code separation

---
+++ PluginsAndFeatures/intellij/platform/build.gradle.kts
@@ -0,0 +1,34 @@
+import org.gradle.api.internal.HasConvention
+import org.jetbrains.intellij.IntelliJPlugin
+import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformJvmPlugin
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+
+apply<KotlinPlatformJvmPlugin>()
+apply<IntelliJPlugin>()
+
+val SourceSet.kotlin: SourceDirectorySet
+    get() = (this as HasConvention).convention.getPlugin<KotlinSourceSet>().kotlin
+
+operator fun File.div(part: String) = File(this, part)
+
+val path = File(project.projectDir, "../../azure-toolkit-for-intellij").canonicalFile!!
+if (!path.isDirectory) error("No directory at $path")
+
+java.sourceSets["main"].java.setSrcDirs(listOf(path / "src"))
+java.sourceSets["main"].kotlin.setSrcDirs(listOf(path / "src"))
+java.sourceSets["main"].resources.setSrcDirs(listOf(path / "resources"))
+
+@Suppress("PropertyName")
+val intellij_version: String by project
+
+val sources: String by project
+
+val dep_plugins: String by project
+
+intellij {
+    // this.setPlugins("maven", dep_plugins)
+    type = "RD"
+    version = "RD-2018.1"
+    // version = intellij_version
+    downloadSources = sources.toBooleanChecked()
+}
