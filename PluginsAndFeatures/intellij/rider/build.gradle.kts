1f532fd0fab0f1648afacda14a9433611835de8d
Evgeniy.Stepanov <Evgeniy.Stepanov@JetBrains.com>
2018-04-30 22:03:11 +0200

    Introduce separate xml plugin definitions

--- PluginsAndFeatures/intellij/rider/build.gradle.kts
+++ PluginsAndFeatures/intellij/rider/build.gradle.kts
@@ -1,7 +1,17 @@
-plugins {
-    kotlin("jvm")
+import org.jetbrains.intellij.IntelliJPlugin
+import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformJvmPlugin
+
+apply<KotlinPlatformJvmPlugin>()
+apply<IntelliJPlugin>()
+
+val sources: String by project
+
+intellij {
+    type = "RD"
+    version = "RD-2018.1"
+    downloadSources = sources.toBooleanChecked()
 }
 
 dependencies {
-    compile(kotlin("stdlib"))
+    compile(project(":platform"))
 }
